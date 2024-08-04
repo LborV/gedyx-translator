9263021a3a97401a1a325e3e15f0d713b4612e32
lborv <m@m.lv>
2024-08-04 17:41:05 +0300

    Add copy-static-files script and get HTML file content

--- apps/api/src/controllers/api.controller.ts
+++ apps/api/src/controllers/api.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Get, Param, Put } from '@nestjs/common'
 import { CompaniesService } from '../services/companies.service'
 import { ProjectsService } from '../services/projects.service'
 import { DocumentsService } from '../services/documents.service'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 
 @Controller('api')
 export class ApiController {
@@ -19,6 +21,13 @@ export class ApiController {
   @Put('company')
   async company(@Body() data: { name: string }) {
     return this.companiesService.create(data)
+  }
+
+  @Get()
+  async get() {
+    const htmlFilePath = join(__dirname, '..', 'api', 'src', 'html', 'index.html');
+
+    return readFileSync(htmlFilePath, 'utf8')
   }
 
   @Get('company')
